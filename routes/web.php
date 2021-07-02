112b8b9708ee725b51378810a61d60a5aa913308
Cesar Romero <cesareromerog88@gmail.com>
2021-07-02 02:05:23 -0400

    Agregando migraciones, primer model y AM del empleado.-

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,44 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return redirect('empleados');
+    //return view('welcome');
+});
+
+Route::group(['prefix' => 'empleados'], function() {
+	Route::get('/', [
+	    'uses' => 'EmpleadoController@index',
+	    'as' => 'index'
+	    ]
+	)->name('index_empleado');
+
+    Route::get('/crear', [
+        'uses' => 'EmpleadoController@crear',
+        'as' => 'crear'
+        ]
+    );
+
+    Route::post('/guardar', [
+        'uses' => 'EmpleadoController@guardar',
+        'as' => 'guardar'
+        ]
+    );
+
+    Route::get('/editar/{empleadoId}', [
+        'uses' => 'EmpleadoController@editar',
+        'as' => 'editar'
+        ]
+    );
+
+    Route::put('/actualizar', [
+        'uses' => 'EmpleadoController@actualizar',
+        'as' => 'actualizar'
+        ]
+    );
+
+    Route::delete('/{empleadoId}', [
+        'uses' => 'EmpleadoController@eliminar',
+        'as' => 'eliminar'
+        ]
+    );
 });
